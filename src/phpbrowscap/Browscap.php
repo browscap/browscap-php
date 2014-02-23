e23e30aa5be5867e46e6121a2d1dc94c8a0eae36
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-02-23 13:23:45 +0100

    extracted Exception to its own file

--- src/phpbrowscap/Browscap.php
+++ src/phpbrowscap/Browscap.php
@@ -1063,18 +1063,3 @@ class Browscap
         return $ua;
     }
 }
-
-/**
- * Browscap.ini parsing class exception
- *
- * @package    Browscap
- * @author     Jonathan Stoppani <jonathan@stoppani.name>
- * @copyright  Copyright (c) 2006-2012 Jonathan Stoppani
- * @version    1.0
- * @license    http://www.opensource.org/licenses/MIT MIT License
- * @link       https://github.com/GaretJax/phpbrowscap/
- */
-class Exception extends \Exception
-{
-    // nothing to do here
-}
