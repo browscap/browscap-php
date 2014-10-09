882891b1cff20462bb3f4cd13d0bb5bb8d46c3f0
James Titcumb <james@asgrim.com>
2014-10-09 08:40:17 +0100

    Bump version constant to 2.0.4 ready for release

--- src/phpbrowscap/Browscap.php
+++ src/phpbrowscap/Browscap.php
@@ -41,9 +41,9 @@ class Browscap
     /**
      * Current version of the class.
      */
-    const VERSION = '2.0.3';
+    const VERSION = '2.0.4';
 
-    const CACHE_FILE_VERSION = '2.0.3';
+    const CACHE_FILE_VERSION = '2.0.4';
 
     /**
      * Different ways to access remote and local files.
