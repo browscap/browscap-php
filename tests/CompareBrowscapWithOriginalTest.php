62d69c3175fd2d2243e801a42c7a7f12e2a0d8e3
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-12-15 20:55:30 +0100

    fix Test

--- tests/CompareBrowscapWithOriginalTest.php
+++ tests/CompareBrowscapWithOriginalTest.php
@@ -29,7 +29,55 @@ class CompareBrowscapWithOriginalTest extends \PHPUnit_Framework_TestCase
     /**
      * @var array
      */
-    private $properties = array();
+    private $properties = array(
+        'browser_name_regex'          => null,
+        'browser_name_pattern'        => null,
+        'Parent'                      => null,
+        'Comment'                     => 'Default Browser',
+        'Browser'                     => 'Default Browser',
+        'Browser_Type'                => 'unknown',
+        'Browser_Bits'                => '0',
+        'Browser_Maker'               => 'unknown',
+        'Browser_Modus'               => 'unknown',
+        'Version'                     => '0.0',
+        'MajorVer'                    => '0',
+        'MinorVer'                    => '0',
+        'Platform'                    => 'unknown',
+        'Platform_Version'            => 'unknown',
+        'Platform_Description'        => 'unknown',
+        'Platform_Bits'               => '0',
+        'Platform_Maker'              => 'unknown',
+        'Alpha'                       => 'false',
+        'Beta'                        => 'false',
+        'Win16'                       => 'false',
+        'Win32'                       => 'false',
+        'Win64'                       => 'false',
+        'Frames'                      => 'false',
+        'IFrames'                     => 'false',
+        'Tables'                      => 'false',
+        'Cookies'                     => 'false',
+        'BackgroundSounds'            => 'false',
+        'JavaScript'                  => 'false',
+        'VBScript'                    => 'false',
+        'JavaApplets'                 => 'false',
+        'ActiveXControls'             => 'false',
+        'isMobileDevice'              => 'false',
+        'isTablet'                    => 'false',
+        'isSyndicationReader'         => 'false',
+        'Crawler'                     => 'false',
+        'CssVersion'                  => '0',
+        'AolVersion'                  => '0',
+        'Device_Name'                 => 'unknown',
+        'Device_Maker'                => 'unknown',
+        'Device_Type'                 => 'unknown',
+        'Device_Pointing_Method'      => 'unknown',
+        'Device_Code_Name'            => 'unknown',
+        'Device_Brand_Name'           => 'unknown',
+        'RenderingEngine_Name'        => 'unknown',
+        'RenderingEngine_Version'     => 'unknown',
+        'RenderingEngine_Description' => 'unknown',
+        'RenderingEngine_Maker'       => 'unknown',
+    );
 
     /**
      * This method is called before the first test of this test class is run.
@@ -63,8 +111,8 @@ class CompareBrowscapWithOriginalTest extends \PHPUnit_Framework_TestCase
 
         $doNotCompare = array('renderingengine_description');
 
-        $libPropertyKeys = array_diff(array_map('strtolower', array_keys($libProperties)), $doNotCompare);
-        $bcPropertyKeys  = array_diff(array_map('strtolower', array_keys($bcProperties)), $doNotCompare);
+        $libPropertyKeys = array_diff(array_keys($libProperties), $doNotCompare);
+        $bcPropertyKeys  = array_diff(array_keys($bcProperties), $doNotCompare);
 
         self::assertSame(
             $libPropertyKeys,
@@ -78,10 +126,6 @@ class CompareBrowscapWithOriginalTest extends \PHPUnit_Framework_TestCase
                 $libProperties,
                 'Property `' . $bcProp . '` from Browscap doesn\'t match anything in get_browser.'
             );
-
-            if ('browser_name_regex' != $bcProp) {
-                $this->properties[$bcProp] = strtolower($bcProp);
-            }
 
             unset($libProperties[strtolower($bcProp)]);
         }
@@ -105,23 +149,17 @@ class CompareBrowscapWithOriginalTest extends \PHPUnit_Framework_TestCase
         $libResult = get_browser($userAgent);
         $bcResult  = self::$object->getBrowser($userAgent);
 
-        if ($userAgent == Ini::BROWSCAP_VERSION_KEY) {
-            self::assertSame(
-                $libResult->version,
-                self::$object->getSourceVersion(),
-                'Source file version incorrect: ' . $libResult->version
-            );
-        } else {
-            foreach ($this->properties as $bcProp => $libProp) {
-                $libValue = $libResult->{$libProp};
-                $bcValue  = $bcResult->{$bcProp};
+        foreach (array_keys($this->properties) as $bcProp) {
+            $bcProp = strtolower($bcProp);
 
-                self::assertSame(
-                    $libValue,
-                    $bcValue,
-                    $bcProp . ': ' . $libValue . ' != ' . $bcValue
-                );
-            }
+            $libValue = $libResult->{$bcProp};
+            $bcValue  = $bcResult->{$bcProp};
+
+            self::assertSame(
+                $libValue,
+                $bcValue,
+                $bcProp . ': ' . $libValue . ' != ' . $bcValue
+            );
         }
     }
 
