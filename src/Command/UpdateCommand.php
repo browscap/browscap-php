609adf3ff5217d688e2f2f64f7ab60169b6d26ba
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2015-11-22 17:05:13 +0100

    #122: remove unused statement

--- src/Command/UpdateCommand.php
+++ src/Command/UpdateCommand.php
@@ -35,7 +35,6 @@ use BrowscapPHP\Cache\BrowscapCacheInterface;
 use BrowscapPHP\Helper\IniLoader;
 use BrowscapPHP\Helper\LoggerHelper;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
