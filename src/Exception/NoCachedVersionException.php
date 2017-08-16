dfeb4f71887e12ec3ae746fd17cbf0a75b2f1633
Thomas Müller <thomas.mueller@geld.de>
2017-08-16 10:02:52 +0200

    #216: change update check

---
+++ src/Exception/NoCachedVersionException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types = 1);
+
+namespace BrowscapPHP\Exception;
+
+/**
+ * Exception to handle errors while fetching a remote file
+ */
+final class NoCachedVersionException extends DomainException
+{
+}
